136432a3bd0699692db80e853cca4cde89b0d8ab
Ashraf <achreffaidiachref@gmail.com>
2020-05-16 19:10:22 +0200

    improve TimeLineUI

--- lib/screens/EventRequestsUI.dart
+++ lib/screens/EventRequestsUI.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:testing_app/Api/Request/request.dart';
 import 'package:testing_app/Api/staff/staff.dart';
 import 'package:testing_app/Consts/Strings.dart';
 import 'package:testing_app/Widgets/RequestPopUp.dart';
+import 'package:testing_app/Widgets/charts/requestsCircularChart.dart';
 import 'package:testing_app/Widgets/staffPopUp.dart';
+import 'dart:html' as html;
+
 
 class EventRequestsUI extends StatefulWidget {
   String event ;
@@ -22,13 +26,16 @@ class _EventRequestsUIState extends State<EventRequestsUI> {
   List<RequestElement> _requests = new List();
   List<String> _permissions = new List();
   String event  ;
-  List<String> requestState = ["Pending","Unpaid","Accepted","Refused"];
+
+  bool _sort = false ;
+
   _loadRequests(){
     http.get(baseUrl+"api/event/request",headers: {
       "event":event
     }).then((http.Response response){
       print(response.body);
       _requests = requestsFromJson(response.body).requests;
+
       setState(() {
 
       });
@@ -48,57 +55,144 @@ class _EventRequestsUIState extends State<EventRequestsUI> {
       appBar: AppBar(title: Text("Requests"),leading: Container(),actions: <Widget>[
         FlatButton.icon(onPressed: (){
           _loadRequests();
-        }, icon: Icon(Icons.refresh), label: Text("Refresh") , color: Colors.green,)
+        }, icon: Icon(Icons.refresh,color: Colors.green,), label: Text("Refresh" , style: TextStyle(color: Colors.green),) ,color: Colors.white),
+        FlatButton.icon(onPressed: (){
+          exportData();
+        }, icon: Icon(Icons.file_download, color: Colors.deepOrange,), label: Text("Export as CSV" , style: TextStyle(color: Colors.deepOrange),) ,color: Colors.white,)
+
+
       ],),
       body: _getBody(),
     );
   }
 
+
+  void exportData(){
+    List<List<dynamic>> temp = [];
+    temp.add(["name","plan","cost","state"]) ;
+    _requests.forEach((r){
+      temp.add([r.user.name,r.plan.name,r.plan.cost,requestState[r.request.state]]) ;
+    });
+    String csv = const ListToCsvConverter().convert(temp);
+
+// prepare
+    final bytes = utf8.encode(csv);
+    final blob = html.Blob([bytes]);
+    final url = html.Url.createObjectUrlFromBlob(blob);
+    final anchor = html.document.createElement('a') as html.AnchorElement
+      ..href = url
+      ..style.display = 'none'
+      ..download = "requests_"+DateTime.now().toIso8601String()+ ".csv";
+    html.document.body.children.add(anchor);
+
+// download
+    anchor.click();
+
+// cleanup
+    html.document.body.children.remove(anchor);
+    html.Url.revokeObjectUrl(url);
+  }
+
   _getBody() {
     return Container(
-      child: ListView.builder(
-          itemCount: _requests.length,
-          itemBuilder: _requestItemBuilder)
-    );
+      child: Row(
+        children: <Widget>[
+          Card(child: Container( width: 600, child: generateTable())),
+          Card(child: Container(
+            width: 400.0,
+            height: 400,
+            child: DatumLegendRequestsState.withRealData(_requests),),)
+        ],
+      ));
   }
 
 
 
-  Widget _requestItemBuilder(BuildContext context, int index) {
+  Widget generateTable(){
 
-    return Card(
-      child :
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(_requests[index].plan.name , style: TextStyle(fontSize: 23 ,fontWeight: FontWeight.bold),),
-                  Text(_requests[index].user.name , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
-                  Text(_requests[index].plan.cost.toString()+" TND" , style: TextStyle(color: Colors.green ,fontSize: 18 ,fontWeight: FontWeight.bold),),
-                  Text("Request State :"+requestState[_requests[index].request.state]),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  RaisedButton.icon(onPressed: (){
-                    _updateRequest(_requests[index]);
-                  }, icon: Icon(Icons.update), label: Text("Update")),
-                  RaisedButton.icon(onPressed: (){
-                    _deleteRequest(_requests[index]) ;
-                  }, icon: Icon(Icons.delete), label: Text("Delete")),
-                ],
-              )
-            ],
-          ),
-        ),
-      )
+
+    return PaginatedDataTable(
+      source: RequestsDataSource(_requests,_updateRequest),
+      header: Text("Table of Requests"),
+      rowsPerPage: 10,
+      columns: [
+
+        DataColumn(
+            onSort: onSort,
+            label: Text(
+              "",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            onSort: onSort,
+            label: Text(
+              "Name",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            onSort: onSort,
+            label: Text(
+              "Plan",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            onSort: onSort,
+          numeric: true,
+
+            label: Text(
+              "Cost",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            onSort: onSort,
+            label: Text(
+              "State",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+      ],
     );
+
   }
+
+  onSort(int columnIndex, bool ascending) {
+
+    ascending = _sort ;
+    _sort= !_sort;
+
+
+     if (columnIndex == 1) {
+      if (ascending) {
+        _requests.sort((a, b) => a.user.name.compareTo(b.user.name));
+      } else {
+        _requests.sort((a, b) => b.user.name.compareTo(a.user.name));
+      }
+    } else if (columnIndex ==2) {
+      if (ascending) {
+        _requests.sort((a, b) => a.plan.name.compareTo(b.plan.name));
+      } else {
+        _requests.sort((a, b) => b.plan.name.compareTo(a.plan.name));
+      }
+    }else if (columnIndex == 3) {
+      if (ascending) {
+        _requests.sort((a, b) => a.plan.cost.compareTo(b.plan.cost));
+      } else {
+        _requests.sort((a, b) => b.plan.cost.compareTo(a.plan.cost));
+      }
+    }
+    else if (columnIndex == 4) {
+      if (ascending) {
+        _requests.sort((a, b) => a.request.state.compareTo(b.request.state));
+      } else {
+        _requests.sort((a, b) => b.request.state.compareTo(a.request.state));
+      }
+    }
+
+    setState(() {
+
+    });
+  }
+
+
 
 
   _updateRequest(RequestElement requestElement){
@@ -127,6 +221,61 @@ class _EventRequestsUIState extends State<EventRequestsUI> {
 _loadRequests();
     });
   }
+
+
+}
+
+
+class RequestsDataSource extends DataTableSource {
+
+
+  final List<RequestElement> _results;
+  final Function _updateRequest ;
+  RequestsDataSource(this._results,this._updateRequest);
+
+  void _sort<T>(Comparable<T> getField(RequestElement d), bool ascending) {
+    _results.sort((RequestElement a, RequestElement b) {
+      if (!ascending) {
+        final RequestElement c = a;
+        a = b;
+        b = c;
+      }
+      final Comparable<T> aValue = getField(a);
+      final Comparable<T> bValue = getField(b);
+      return Comparable.compare(aValue, bValue);
+    });
+    notifyListeners();
+  }
+
+  int _selectedCount = 0;
+
+  @override
+  DataRow getRow(int index) {
+    assert(index >= 0);
+    if (index >= _results.length) return null;
+    final RequestElement result = _results[index];
+    return DataRow.byIndex(
+        index: index,
+        cells: <DataCell>[
+          DataCell(Text(index.toString())),
+          DataCell(Text('${result.user.name}')),
+          DataCell(Text('${result.plan.name}')),
+          DataCell(Text('${result.plan.cost} TND')),
+          DataCell(Text(requestState[result.request.state]),showEditIcon: true,onTap: (){
+            _updateRequest(result);
+          })
+
+        ]);
+  }
+
+  @override
+  int get rowCount => _results.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedCount;
 
 
 }
--- lib/screens/EventTimeLineUI.dart
+++ lib/screens/EventTimeLineUI.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:html' as html;
 import 'package:testing_app/Api/Request/request.dart';
 import 'package:testing_app/Api/TimeSlots/timeslots.dart';
 import 'package:testing_app/Api/staff/staff.dart';
@@ -9,6 +11,10 @@ import 'package:testing_app/Consts/Strings.dart';
 import 'package:testing_app/Widgets/RequestPopUp.dart';
 import 'package:testing_app/Widgets/TimeSlotPopUp.dart';
 import 'package:testing_app/Widgets/staffPopUp.dart';
+import 'package:timeline_list/timeline.dart';
+import 'package:timeline_list/timeline_model.dart';
+import 'package:intl/intl.dart';
+
 
 class EventTimeLineUI extends StatefulWidget {
   String event ;
@@ -22,6 +28,9 @@ class _EventTimeLineUIState extends State<EventTimeLineUI> {
 
   _EventTimeLineUIState(this.event) ;
 
+  final m = new DateFormat('MMMEd');
+  final d = new DateFormat('jm');
+
   String event  ;
   List<Timeslot> timeslots;
 
@@ -31,6 +40,8 @@ class _EventTimeLineUIState extends State<EventTimeLineUI> {
     }).then((http.Response response){
       print(response.body);
       timeslots =  timeSlotsFromJson(response.body).timeslots;
+      timeslots.sort((Timeslot a, Timeslot b) => a.startDate.compareTo(b.startDate) );
+
       setState(() {
 
       });
@@ -51,21 +62,191 @@ class _EventTimeLineUIState extends State<EventTimeLineUI> {
       appBar: AppBar(title: Text("Time Line"),leading: Container(),actions: <Widget>[
         FlatButton.icon(onPressed: (){
           _addTimeSlot();
-        }, icon: Icon(Icons.add), label: Text("Add TimeSlot") , color: Colors.green,)
+        }, icon: Icon(Icons.add_circle,color: Colors.green,), label: Text("Add TimeSlot" , style: TextStyle(color: Colors.green),) ,color: Colors.white),
+        FlatButton.icon(onPressed: (){
+          exportData();
+        }, icon: Icon(Icons.file_download, color: Colors.deepOrange,), label: Text("Export as CSV" , style: TextStyle(color: Colors.deepOrange),) ,color: Colors.white,)
+
       ],),
       body: _getBody(),
     );
   }
 
+
+  void exportData(){
+    List<List<dynamic>> temp = [];
+    temp.add(["title","location","start_time_Iso8601","end_time_Iso8601","start_day",'start_time','end_time']) ;
+    timeslots.forEach((r){
+      temp.add([r.title,r.location,r.startDate.toIso8601String(),r.endDate.toIso8601String(),m.format(r.startDate),d.format(r.startDate),d.format(r.startDate)],) ;
+    });
+    String csv = const ListToCsvConverter().convert(temp);
+
+// prepare
+    final bytes = utf8.encode(csv);
+    final blob = html.Blob([bytes]);
+    final url = html.Url.createObjectUrlFromBlob(blob);
+    final anchor = html.document.createElement('a') as html.AnchorElement
+      ..href = url
+      ..style.display = 'none'
+      ..download = "timelien_"+DateTime.now().toIso8601String()+ ".csv";
+    html.document.body.children.add(anchor);
+
+// download
+    anchor.click();
+
+// cleanup
+    html.document.body.children.remove(anchor);
+    html.Url.revokeObjectUrl(url);
+  }
+
+
   _getBody() {
+
+    double width = MediaQuery.of(context).size.width*0.4 ;
     return Container(
-      child: ListView.builder(
-          itemCount: timeslots.length,
-          itemBuilder: _requestItemBuilder)
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: width,
+              child: generateTable(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    width: width,
+                    child : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical :20.0,horizontal: 20),
+                          child: Text("TimeLine Preview",style: TextStyle(fontSize: 26 , color: Colors.blueGrey),),
+                        ),
+                        Container(
+                            height: 700,
+                            child: _getTimeLine()),
+                      ],
+                    )),
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 
 
+  Widget _getTimeLine(){
+    double width = 300 ;
+
+    List<TimelineModel> temp = new List();
+
+
+      int day = 0;
+      int month =0 ;
+
+    if(timeslots.isNotEmpty) timeslots.forEach((t){
+
+      if(t.startDate.day!=day||t.startDate.month!=month){
+
+        day = t.startDate.day ;
+        month = t.startDate.month ;
+        temp.add(
+            (TimelineModel(Card(
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [Colors.blue, Colors.blueGrey])),
+                width: width,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(m.format(t.startDate) , style: TextStyle(fontSize: 22 , fontWeight: FontWeight.bold ,color: Colors.white),),
+                )
+
+              ),
+            ),
+                position: TimelineItemPosition.left,
+                iconBackground: Colors.blue,
+                icon: Icon(Icons.stars))
+        ));
+      }
+      temp.add(TimelineModel(Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: width,
+            child: Column(
+              children: <Widget>[
+                Text(t.title , style: TextStyle(fontSize: 23 ,fontWeight: FontWeight.bold),),
+                Text(t.location , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
+                Text(d.format(t.startDate) +" to " +d.format(t.endDate), style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
+              ],
+            ),
+          ),
+        ),
+      ),
+          position: TimelineItemPosition.right,
+          iconBackground: Colors.purpleAccent,
+          icon: Icon(Icons.blur_circular)));
+    });
+
+    return Timeline(children: temp, position: TimelinePosition.Center ,shrinkWrap: true,);
+
+
+  }
+
+  Widget generateTable(){
+
+
+    return PaginatedDataTable(
+      source: TimeSlotsDataSource(timeslots,_deleteTimeSlot),
+      header: Text("Table of Requests"),
+      rowsPerPage: 10,
+      columns: [
+
+        DataColumn(
+            label: Text(
+              "Title",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            label: Text(
+              "Location",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            label: Text(
+              "Start Day",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            numeric: true,
+
+            label: Text(
+              "start",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            label: Text(
+              "end",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+        DataColumn(
+            label: Text(
+              "Delete",
+              style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w900),
+            )),
+      ],
+    );
+
+  }
 
   Widget _requestItemBuilder(BuildContext context, int index) {
 
@@ -82,8 +263,8 @@ class _EventTimeLineUIState extends State<EventTimeLineUI> {
                 children: <Widget>[
                   Text(timeslots[index].title , style: TextStyle(fontSize: 23 ,fontWeight: FontWeight.bold),),
                   Text(timeslots[index].location , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
-                  Text(timeslots[index].startDate.toIso8601String() , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
-                  Text(timeslots[index].endDate.toIso8601String() , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
+                  Text("start : " +m.format(timeslots[index].startDate)+" "+d.format(timeslots[index].startDate) , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
+                  Text("end : " +m.format(timeslots[index].endDate)+" "+d.format(timeslots[index].endDate) , style: TextStyle(fontSize: 18 ,fontWeight: FontWeight.bold),),
                 ],
               ),
               Column(
@@ -128,3 +309,51 @@ _loadTimeSlots();
 
 
 }
+
+
+class TimeSlotsDataSource extends DataTableSource {
+  final m = new DateFormat('MMMEd');
+  final d = new DateFormat('jm');
+
+
+  final List<Timeslot> _results;
+  final Function _deleteTimeSlot ;
+  TimeSlotsDataSource(this._results,this._deleteTimeSlot);
+
+  void _sort<T>(Comparable<T> getField(RequestElement d), bool ascending) {
+    notifyListeners();
+  }
+
+  int _selectedCount = 0;
+
+  @override
+  DataRow getRow(int index) {
+    assert(index >= 0);
+    if (index >= _results.length) return null;
+    final Timeslot result = _results[index];
+    return DataRow.byIndex(
+        index: index,
+        cells: <DataCell>[
+          DataCell(Text('${result.title}',style: TextStyle(fontWeight: FontWeight.bold),)),
+          DataCell(Text('${result.location}')),
+          DataCell(Text('${m.format(result.startDate)}')),
+          DataCell(Text('${d.format(result.startDate)}')),
+          DataCell(Text('${d.format(result.endDate)}')),
+          DataCell(Text('delete' , style: TextStyle(color:Colors.red),),onTap:(){
+            _deleteTimeSlot(result);
+          }),
+        ]);
+  }
+
+  @override
+  int get rowCount => _results.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedCount;
+
+
+}
+
